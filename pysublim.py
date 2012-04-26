4a435ad6bdc9552af61bf393e309aff0b0793e3b
DJ Amireh <cactusbin@gmail.com>
2012-04-25 22:13:40 -0400

    Add configuration file for screen size. Prevent text from going offscreen.

--- pysublim.py
+++ pysublim.py
@@ -1,8 +1,7 @@
 import pyosd
 import argparse
+import ConfigParser
 import subprocess
-
-osd = pyosd.osd()
 
 def displaySublim(string, delay):
     import random
@@ -10,29 +9,34 @@ def displaySublim(string, delay):
 
     global osd
 
-    osd.set_vertical_offset(random.randint(0, screenHeight))
-    osd.set_horizontal_offset(random.randint(0, screenWidth))
+    osd.set_vertical_offset(random.randint(0, (screenHeight - 50)))
+    osd.set_horizontal_offset(random.randint(0, (screenWidth - 50)))
     osd.show()
     osd.display(string)
     time.sleep(delay)
     osd.hide()
 
-parser = argparse.ArgumentParser(description = "Python script to display subliminal messages")
-parser.add_argument("File")
-parser.add_argument("-c", "--color", help = "Font color (default: black)", default = "black")
-parser.add_argument("-s", "--size", help = "Font size (default: 25)", default = "25")
-parser.add_argument("-t", "--time", type = float, help = "Time in seconds the message will be displayed (deafult: .05)", default = .05)
-parser.add_argument("-l", "--loop", help = "Use this option if you wish to loop subliminals until killed", action = "store_true")
-parser.add_argument("-e", "--execute", help = "Use if you want to execute a command instead of reading from a file", action = "store_true")
-parser.add_argument("-ef", "--executefile", help = "Use if you want to execute a list of commands from a file", action = "store_true")
+osd = pyosd.osd()
 
-args = parser.parse_args()
+configparser = ConfigParser.ConfigParser()
+configparser.readfp(open("pysublim.cfg"))
+
+argparser = argparse.ArgumentParser(description = "Python script to display subliminal messages")
+argparser.add_argument("File")
+argparser.add_argument("-c", "--color", help = "Font color (default: black)", default = "black")
+argparser.add_argument("-s", "--size", help = "Font size (default: 25)", default = "25")
+argparser.add_argument("-t", "--time", type = float, help = "Time in seconds the message will be displayed (deafult: .05)", default = .05)
+argparser.add_argument("-l", "--loop", help = "Use this option if you wish to loop subliminals until killed", action = "store_true")
+argparser.add_argument("-e", "--execute", help = "Use if you want to execute a command instead of reading from a file", action = "store_true")
+argparser.add_argument("-ef", "--executefile", help = "Use if you want to execute a list of commands from a file", action = "store_true")
+
+args = argparser.parse_args()
 
 osd.set_colour(args.color)
 osd.set_font("-*-*-*-*-*-*-" + args.size + "-*-*-*-*-*-*-*")
 
-screenWidth = 1680
-screenHeight = 1050
+screenWidth = configparser.getint("screen", "width")
+screenHeight = configparser.getint("screen", "height")
 
 if args.execute and args.executefile:
     print "Conflicting options 'execute' and 'executefile'"
